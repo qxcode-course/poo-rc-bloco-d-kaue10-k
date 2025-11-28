b2abee1f6b988dbf4e332b74f4093ed9221f9139
kaue10-k <limak0655@gmail.com>
2025-11-28 13:36:31 +0000

    trabalho iniciado

---
+++ tasks/poo/agenda/py/draft.py
@@ -0,0 +1,19 @@
+class Telefone:
+    def __init__ (self, id, numero):
+        self.id = id 
+        self.numero = numero
+
+    def __str__(self):
+        return f"{self.id}:{self.numero}"
+
+
+class Contato:
+    def __init__(self, nome):
+        self.nome = nome
+        self.telefone = []
+        self.favorito = false 
+        jjhdhhhhhhhhhhhhyhghjgghghhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhgghkgkj
+fsdfhsgdsdskjdfskjdfgsfgsjfgsjfsgffsfgsgfksdjfgsdkfgjgkjgdsjkdfjjjddjffjfjffjfjjjfghghsvdjsfvsjdvfsjdvfsjhvjhvhjvjjjjjjjjjjjvvvjfddhhdhdfddkfhfghjdfhjfgjfdgdfjkhdhdjddfjdjkdjhhdskjdjhsddsjhhhjdsvsdjhhhdhjdssdjhjjhdsdshffffhfhfhfdjhfjbjvjjvjbdkbvjxcjvjxcbcjbkcvjbjbcjbbjcxbjcbcxjbjbcxbjjbcvjbvjbxckbvjbxcbjkkbcxbjbjcxjvjxcjbcjbcxkjkjvjkzxcjbjkckbxckjjbjbkcvj
+    
+
+    
