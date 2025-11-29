33a0de647b744cef49374ed18d56200dbb214fd3
kaue10-k <limak0655@gmail.com>
2025-11-29 14:55:50 +0000

    ..

--- tasks/poo/agenda/py/draft.py
+++ tasks/poo/agenda/py/draft.py
@@ -12,8 +12,22 @@ class Contato:
         self.nome = nome
         self.telefone = []
         self.favorito = false 
-        jjhdhhhhhhhhhhhhyhghjgghghhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhgghkgkj
-fsdfhsgdsdskjdfskjdfgsfgsjfgsjfsgffsfgsgfksdjfgsdkfgjgkjgdsjkdfjjjddjffjfjffjfjjjfghghsvdjsfvsjdvfsjdvfsjhvjhvhjvjjjjjjjjjjjvvvjfddhhdhdfddkfhfghjdfhjfgjfdgdfjkhdhdjddfjdjkdjhhdskjdjhsddsjhhhjdsvsdjhhhdhjdssdjhjjhdsdshffffhfhfhfdjhfjbjvjjvjbdkbvjxcjvjxcbcjbkcvjbjbcjbbjcxbjcbcxjbjbcxbjjbcvjbvjbxckbvjbxcbjkkbcxbjbjcxjvjxcjbcjbcxkjkjvjkzxcjbjkckbxckjjbjbkcvj
+        
+
+    def ad_telefone(self, id, numero):
+        self.telefone.append(telefone(id, numero))
+
+    def remover_telefone(self, id):
+        self.telefone = [t for t in self.telefone if t.id != id]
+
+
+    def __str__(self):
+        fav = "*" if self.favorito else ""
+        tel = ",".join(str(t) for t in self.telefone)
+        return f"[{fav}] {self.nome} -> {tel}" 
+        def 
+
+
     
 
     
