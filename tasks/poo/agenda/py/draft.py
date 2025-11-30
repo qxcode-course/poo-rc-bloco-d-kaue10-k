89b3daba993af9d039cf054469675fc5abaed52e
kaue10-k <limak0655@gmail.com>
2025-11-30 11:05:24 +0000

    ..

--- tasks/poo/agenda/py/draft.py
+++ tasks/poo/agenda/py/draft.py
@@ -25,7 +25,40 @@ class Contato:
         fav = "*" if self.favorito else ""
         tel = ",".join(str(t) for t in self.telefone)
         return f"[{fav}] {self.nome} -> {tel}" 
-        def 
+    
+
+class Agenda:
+    def __init__(self):
+        self.contatos = {}
+
+    def add_contato(self, nome, telefone):
+        if nome not in self.contatos:
+            self.contato[nome] = contato = (nome)
+
+        for id numero in telefone():
+            self.contato[nome].add_telefone(id, numero)
+
+
+    def mostrar(self):
+        for nome in sorted(self.contato):
+            print(self.contato[nome])
+
+
+    def remover_contato(self):
+        if nome in self.contato:
+            del self.contato[nome]
+
+
+    def remover_telefone(self, nome, id):
+        if nome in self.contato:
+            self.contato[nome].remover_telefone(id)
+    
+
+
+
+
+
+
 
 
     
